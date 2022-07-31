b0505225bef9aa7126c5ab9151184fe4bd33f610
AntropovNikita <nikitaantropov@inbox.ru>
2022-07-31 20:32:01 +0300

    Создан класс FileReader с исключениями

---
+++ Model/modelexception.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <exception>
+
+
+class WrongFileNameException : public std::exception
+{
+	private:
+		std::string __message;
+
+	public:
+		WrongFileNameException(const std::string& msg) : __message(msg) {}
+
+		std::string what() { return "Wrong file name: " + this->__message; }
+};
+
+class NoSuchFileException : public std::exception
+{
+	private:
+		std::string __message;
+
+	public:
+		NoSuchFileException(const std::string& msg) : __message(msg) {}
+
+		std::string what() { return "No such file: " + this->__message; }
+};
+
+class FileOpenException : public std::exception
+{
+	private:
+		std::string __message;
+
+	public:
+		FileOpenException(const std::string& msg) : __message(msg) {}
+
+		std::string what() { return "File open fail: " + this->__message; }
+};
